61d47161ca53035b48ac73a3b6dd259555d85c0c
Douglas <douglassouzacom@gmail.com>
2026-06-01 18:07:53 -0300

    fix instagram login

--- bot.js
+++ bot.js
@@ -9,7 +9,7 @@ const fs = require('fs');
 // =============================================
 //  INSTAGRAM
 // =============================================
-const IG_USER = 'repasseminasbrasil';
+const IG_USER = 'douglassouza.com@gmail.com';
 const IG_PASS = 'Rb@103407';
 
 const ig = new IgApiClient();
